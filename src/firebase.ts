202ed7a9b49320f6503d9d8ab992f547e182b636
NIKHILESH <zonepesgamers@gmail.com>
2025-07-03 21:49:48 +0530

    Done

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,13 +8,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAdsu4RCnJj83jFekvxnJoeW8NsBl5r4H0",
-  authDomain: "finance-manager-app-839a1.firebaseapp.com",
-  projectId: "finance-manager-app-839a1",
-  storageBucket: "finance-manager-app-839a1.firebasestorage.app",
-  messagingSenderId: "491966993352",
-  appId: "1:491966993352:web:f47d880762f1fcac6666fd",
-  measurementId: "G-6RWF3WKSNN"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
